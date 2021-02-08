24580fc7c0c4345d80d3dff0b9050cef90247a9f
gusLopezC <guslopezcallejas@gmail.com>
2021-02-07 19:37:00 -0600

    Frontend

--- app/Models/url.php
+++ app/Models/url.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class url extends Model
+class Url extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = ['url','user_id'];
+
+    public $timestamps = false;
+
+    public function user()
+    {
+	return $this->belongsTo('App\Models\User');
+    }
 }
---
+++ resources/js/shorter.js
@@ -0,0 +1,53 @@
+const { default: axios } = require("axios");
+
+window.shorter = {
+
+    short_url: () => {
+
+        let long_url = document.getElementById('long_url').value;
+
+        if (shorter.validate_url(long_url)) {
+
+            axios.post('/url', {
+                    long_url: long_url
+                })
+                .then(function(response) {
+
+                    document.getElementById('long_url').value = '';
+                    document.getElementById('short_url_container').style.display = "block";
+
+                    document.getElementById('short_url').value = response.data.short_url;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+    },
+
+    copy_url: () => {
+
+        let short_url = document.getElementById('long_url');
+
+        short_url.select();
+        short_url.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+    },
+
+    validate_url: (url) => {
+
+        let status = true;
+
+        if (url.trim() == "") {
+            status = false;
+            alert("Debes ingresar una url");
+        }
+
+        const validUrl = require('valid-url');
+
+        if (!validUrl.isWebUri(url)) {
+            status = false;
+            alert("Url no valida");
+        }
+        return status;
+    },
+}
